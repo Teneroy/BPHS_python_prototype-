66e3f303056bfa24dc0d26f542ca8513d037f187
Teneroy <alexchernishof@gmail.com>
2019-10-27 19:27:14 +0300

    Added voice acting

--- text_reading.py
+++ text_reading.py
@@ -1,6 +1,10 @@
 from PIL import Image
+from gtts import gTTS
 import pytesseract
+import os
 f = open('C:\\BPHS_python_prototype-\\images\\text_photo.txt', 'w')
-text = pytesseract.image_to_string(Image.open('C:\\BPHS_python_prototype-\\images\\td2.jpg'), lang='eng').encode('utf-8')
-print text
+text = pytesseract.image_to_string(Image.open('C:\\BPHS_python_prototype-\\images\\td1.jpg'), lang='eng').encode('utf-8')
 f.write(text)
+tts = gTTS(text=text, lang='en')
+tts.save("pcvoice.mp3")
+os.system("start pcvoice.mp3")
